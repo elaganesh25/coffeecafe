f17b3dba5cf3952ea51032a8d187c14a7b496cee
elaganesh25 <elaganesh25@gmail.com>
2025-06-11 09:50:48 -0500

    Testcase added

--- app1/tests.py
+++ app1/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class Simpleadd(TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
+        self.assertEqual(5 + 5, 10)
+
+class Simplesub(TestCase):
+    def test_subtraction(self):
+        self.assertEqual(1 - 2, -1)
+        self.assertEqual(5 - 10, -5)
